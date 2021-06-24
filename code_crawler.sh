e54f4e9c00d351f1b7ee412d828066829c92daac
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2021-06-24 14:10:25 +0200

    Initial version of dotnet crawler

---
+++ code_crawler.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mydir=${0%/*}
+grep=${GREPCMD:-rg}
+lang="$1"
+shift
+
+cat "$mydir/$lang.config" | grep -v "^$" | grep -v "^#" | while read -r entry; do
+    eval $grep $@ "'\b$entry\b'" .
+done
